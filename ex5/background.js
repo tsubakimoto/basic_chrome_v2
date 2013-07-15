883626de879a812a3b5d5bc027a1bcdcdfdaa700
Yuta Matsumura <dev.tsubaki@gmail.com>
2013-07-15 13:52:50 +0900

    complete #16

--- ex5/background.js
+++ ex5/background.js
@@ -1,5 +1,11 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if (tab.url.indexOf('dotinstall') != -1) {
-		
+		chrome.pageAction.show(tabId);
 	}
+});
+
+chrome.pageAction.onClicked.addListener(function() {
+	chrome.tabs.executeScript(null, {
+		"code": "document.body.style.backgroundColor='red'"
+	});
 });
